b0714dd37fd6a2dfa82b0607b45e09704c246b3b
mparadinha <miguel.p.paradinha@gmail.com>
2023-10-08 21:31:00 +0100

    actually get info from download pages

--- work_script.js
+++ work_script.js
@@ -1,22 +1,37 @@
-function copy_info_to_clipboard() {
-  var info = [];
-  for (var container of document.getElementsByClassName("collection-item-container")) {
-    const download_node = container.getElementsByClassName("redownload-item")[0];
-    const artist_node = container.getElementsByClassName("collection-item-artist")[0];
-    const title_node = container.getElementsByClassName("collection-item-title")[0];
-    info.push({
-      "artist": artist_node.innerText.replace("by ", ""),
-      "title": title_node.innerText.replace("by ", ""),
-      "download_link": download_node.children[0].href,
-    });
-  }
-  navigator.clipboard.writeText(JSON.stringify({"albums": info}));
-  
+var info = [];
+
+async function fetch_download_information() {
+    const collection_items = document.getElementsByClassName("collection-item-container");
+    for (var container of collection_items) {
+        const download_node = container.getElementsByClassName("redownload-item")[0];
+        const download_page_url = download_node.children[0].href;
+        await fetch(download_page_url)
+            .then((response) => response.text())
+            .then((html) => {
+                const page = new DOMParser().parseFromString(html, "text/html");
+                const blob = JSON.parse(page.getElementById("pagedata").getAttribute("data-blob"));
+                info.push({ "download_items": blob["download_items"] });
+            });
+        console.log(info.length);
+    }
 }
 
 var grid = document.getElementsByClassName("collection-items")[0];
-var btn = document.createElement("button");
-btn.innerText = "Copy collection info as JSON to clipboard";
-btn.onclick = copy_info_to_clipboard;
-grid.appendChild(btn);
 
+var fetch_btn = document.createElement("button");
+fetch_btn.innerText = "Fetch collection data";
+fetch_btn.onclick = async function (event) {
+    const saved_btn_text = event.target.innerText;
+    event.target.innerText += " (processing...)";
+    await fetch_download_information();
+
+    var copy_btn = document.createElement("button");
+    copy_btn.innerText = "Copy collection info as JSON to clipboard";
+    copy_btn.onclick = function () {
+        navigator.clipboard.writeText(JSON.stringify({"collection": info}));
+    };
+    grid.appendChild(copy_btn);
+
+    event.target.innerText = saved_btn_text;
+};
+grid.appendChild(fetch_btn);
